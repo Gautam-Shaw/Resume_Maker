b4319037042d2344f50ebae1e11bc1a17fbd502d
Gautam Kumar Shaw <gautamkumarshaw08@gmail.com>
2025-09-10 11:19:48 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -11,7 +11,7 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/users/signup", {
+    const response = await fetch("https://resume-maker-backend-9ygy.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
