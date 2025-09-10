7215d496379fe9bcec53f3438bc4955569028bbd
Gautam Kumar Shaw <gautamkumarshaw08@gmail.com>
2025-09-10 11:19:08 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/users/login", {
+    const response = await fetch("https://resume-maker-backend-9ygy.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
